268ed6103efa25a23cee594945a61fc837caa617
ianhjweng <ianhjweng@outlook.com>
2025-11-06 23:21:13 +0000

    UI Test Polishes #4

--- backend/app.py
+++ backend/app.py
@@ -262,4 +262,4 @@ else:
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
